a1730fc12c051960c1d99ec9b07d0c4f6d18405b
Johnny Bieren <jbieren@redhat.com>
2018-04-09 16:00:19 -0400

    Add s2i
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

---
+++ config/s2i/jenkins/master/configuration/init.groovy
@@ -0,0 +1,35 @@
+import java.util.logging.Logger
+import jenkins.security.s2m.*
+import jenkins.model.*;
+import hudson.markup.RawHtmlMarkupFormatter
+import hudson.model.*
+import hudson.security.*
+
+def logger = Logger.getLogger("")
+logger.info("Disabling CLI over remoting")
+jenkins.CLI.get().setEnabled(false);
+logger.info("Enable Slave -> Master Access Control")
+Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
+// Set global and job read permissions
+def strategy = Jenkins.instance.getAuthorizationStrategy()
+strategy.add(hudson.model.Hudson.READ,'anonymous')
+strategy.add(hudson.model.Item.READ,'anonymous')
+Jenkins.instance.setAuthorizationStrategy(strategy)
+// Set Markup Formatter to Safe HTML so PR hyperlinks work
+Jenkins.instance.setMarkupFormatter(new RawHtmlMarkupFormatter(false))
+Jenkins.instance.save()
+
+logger.info("Setup fedora-fedmsg Messaging Provider")
+FedMsgMessagingProvider fedmsg = new FedMsgMessagingProvider("fedora-fedmsg", "tcp://hub.fedoraproject.org:9940", "tcp://172.19.4.24:9941", "org.fedoraproject");
+GlobalCIConfiguration.get().addMessageProvider(fedmsg)
+
+logger.info("Setup fedora-fedmsg-stage Messaging Provider")
+FedMsgMessagingProvider fedmsgStage = new FedMsgMessagingProvider("fedora-fedmsg-stage", "tcp://stg.fedoraproject.org:9940", "tcp://172.19.4.36:9941", "org.fedoraproject");
+GlobalCIConfiguration.get().addMessageProvider(fedmsgStage)
+
+logger.info("Setup fedora-fedmsg-devel Messaging Provider")
+FedMsgMessagingProvider fedmsgDevel = new FedMsgMessagingProvider("fedora-fedmsg-devel", "tcp://fedmsg-relay.continuous-infra.svc:4001", "tcp://fedmsg-relay.continuous-infra.svc:2003", "org.fedoraproject");
+GlobalCIConfiguration.get().addMessageProvider(fedmsgDevel)
+
+logger.info("Setting Time Zone to be EST")
+System.setProperty('org.apache.commons.jelly.tags.fmt.timeZone', 'America/New_York')
