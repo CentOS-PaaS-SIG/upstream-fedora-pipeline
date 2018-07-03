07aaea09f5590db991312a7659f95aa9ad77a0bd
jstuart <jstuart@redhat.com>
2018-07-03 13:20:58 -0500

    using contra-lib

---
+++ vars/downloadCompose.groovy
@@ -0,0 +1,44 @@
+def call(Map parameters = [:]) {
+    def ciMessage = readJSON text: parameters.get('ciMessage', env.CI_MESSAGE)
+    def imagePrefix = parameters.get('imagePrefix', 'Fedora')
+
+    def compose = ciMessage['compose']
+    def release_version = ciMessage['release_version']
+    def location = ciMessage['location']
+
+    currentBuild.displayName = "${release_version} - ${compose}"
+
+    def imageName = "${imagePrefix}-${release_version}.qcow2"
+
+    sh """
+       rm -f images.json 
+       curl -O ${location}/metadata/images.json
+    """
+
+    def imageJson = readJSON file: images.json
+    def images = imageJson['payload']['images']
+    def imagePath = null
+
+    if (images['Cloud']) {
+        if (images['Cloud']['x86_64']) {
+            images['Cloud']['x86_64'].each { build ->
+                if (build['format'] == 'qcow2') {
+                    imagePath = build['path']
+                }
+            }
+        } else {
+            error("There are no x86_64 images available")
+        }
+    } else {
+        error("There are no cloud images available")
+    }
+
+    if (!imagePath) {
+        error("There are no qcow2 images available")
+    }
+
+    sh("curl -o ${imageName} ${location}/${imgPath}")
+
+    return imageName
+
+}
