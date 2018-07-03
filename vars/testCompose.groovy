d0cd2e4528d25bf2f38e20cb458e04812360341d
jstuart <jstuart@redhat.com>
2018-07-03 13:51:40 -0500

    new command

--- vars/testCompose.groovy
+++ vars/testCompose.groovy
@@ -1,6 +1,7 @@
 def call(Map parameters = [:]) {
     def imageName = parameters.get('imageName')
     sh("curl -O https://pagure.io/upstream-fedora-ci/raw/master/f/fedora-ci-monitor/validate-test-subject.py")
-    sh("rm -rf /tmp/artifacts; python validate-test-subject.py -s ${imageName}")
+    sh("rm -rf /tmp/artifacts")
+    sh("python validate-test-subject.py -s ${imageName}")
     sh("test /tmp/artifacts && false || true")
 }
