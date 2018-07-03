ba13c7496b396198b4a91a228aa02c1ff1ef3791
jstuart <jstuart@redhat.com>
2018-07-03 13:53:42 -0500

    abs

--- vars/testCompose.groovy
+++ vars/testCompose.groovy
@@ -2,6 +2,6 @@ def call(Map parameters = [:]) {
     def imageName = parameters.get('imageName')
     sh("curl -O https://pagure.io/upstream-fedora-ci/raw/master/f/fedora-ci-monitor/validate-test-subject.py")
     sh("rm -rf /tmp/artifacts")
-    sh("python validate-test-subject.py -s ${imageName}")
+    sh("/usr/bin/python validate-test-subject.py -s ${imageName}")
     sh("test /tmp/artifacts && false || true")
 }
