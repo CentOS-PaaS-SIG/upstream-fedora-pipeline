769db205386040913d993d4c2e336f7c40b1d6c8
jstuart <jstuart@redhat.com>
2018-07-03 15:42:23 -0500

    absolute path

--- vars/testCompose.groovy
+++ vars/testCompose.groovy
@@ -1,11 +1,10 @@
 def call(Map parameters = [:]) {
     def imageName = parameters.get('imageName')
     def cmd = """
-              cd /
               curl -O https://pagure.io/upstream-fedora-ci/raw/master/f/fedora-ci-monitor/validate-test-subject.py && \
               rm -rf /tmp/artifacts && \
               pip install requests && \
-              python validate-test-subject.py -s /${imageName} && \
+              python validate-test-subject.py -s \$(pwd)/${imageName} && \
               exit \$?
               """
     executeInContainer(containerName: 'singlehost-test', containerScript: cmd)
