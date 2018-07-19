5df4b03caaab59ceae848568e9019255ead94b52
jstuart <jstuart@redhat.com>
2018-07-19 08:27:47 -0500

    testing env

--- vars/fedoraImageTest.groovy
+++ vars/fedoraImageTest.groovy
@@ -44,6 +44,7 @@ def call(Map parameters = [:]) {
                 docker_repo_url: '172.30.254.79:5000',
                 containers: containers) {
             stage('download image') {
+                sh 'env'
                 imageName = downloadCompose(imageType: imageType)
             }
 
