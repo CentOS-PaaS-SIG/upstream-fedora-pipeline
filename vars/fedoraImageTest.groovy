b089df65801cb8bbbcd6beb45914177229af0b4f
jstuart <jstuart@redhat.com>
2018-08-03 08:59:21 -0500

    testing parameters

--- vars/fedoraImageTest.groovy
+++ vars/fedoraImageTest.groovy
@@ -1,40 +1,7 @@
 def call(Map parameters = [:]) {
 
-    def fedoraRelease = parameters.get('fedoraRelease')
     def imageType = parameters.get('imageType', 'Cloud')
 
-    def libraries = ['contra-lib': ['master', 'https://github.com/openshift/contra-lib.git']]
-
-    libraries.each { name, repo ->
-        library identifier: "${name}@${repo[0]}",
-                retriever: modernSCM([$class: 'GitSCMSource',
-                                      remote: repo[1]])
-
-    }
-
-    properties(
-            [
-                    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '10', daysToKeepStr: '', numToKeepStr: '10')),
-                    pipelineTriggers(
-                            [[
-                                     $class: 'CIBuildTrigger',
-                                     checks: [
-                                             [expectedValue: 'Fedora-Cloud$|Fedora$', field: 'release_short'],
-                                             [expectedValue: 'ga', field: 'release_type'],
-                                             [expectedValue: 'FINISHED', field: 'status'],
-                                             [expectedValue: fedoraRelease, field: 'release_version']
-                                     ],
-                                     providerName: 'fedora-fedmsg', selector: 'topic = "org.fedoraproject.prod.pungi.compose.status.change"'
-                             ]]
-                    ),
-                    parameters(
-                            [
-                                    string(defaultvalue: "", description: 'CI_MESSAGE', name: 'CI_MESSAGE'),
-                                    string(defaultvalue: "", description: 'MESSAGE_HEADERS', name: 'MESSAGE_HEADERS')
-                            ]
-                    )
-            ]
-    )
 
     imageName = null
     containers = ['singlehost-test']
